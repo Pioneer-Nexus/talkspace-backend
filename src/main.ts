56150fb191d191a7291130bcb6e3aa52e2cb0be3
Hoang Hy <hoanghy01122003@gmail.com>
2024-10-16 01:52:43 +0700

    feat: add logging interceptor (TS-14)

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { NestFactory } from "@nestjs/core";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { AppModule } from "./app.module";
 import { IConfigAdapter } from "./infrastructures/config";
+import { LoggerService } from "./infrastructures/logger";
+import { LoggingInterceptor } from "./infrastructures/logger/interceptor";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	const logger = await app.resolve(LoggerService);
+
+	app.useGlobalInterceptors(new LoggingInterceptor(logger));
 	app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
 	const config = app.get(IConfigAdapter);

9956d166603857c0a2ad62eec1524d561de31f16
Hoang Hy <hoanghy01122003@gmail.com>
2024-10-31 01:34:48 +0700

    chore: remove unused resolver

--- src/modules/cat/cat.resolver.ts
+++ src/modules/cat/cat.resolver.ts
@@ -1,17 +1,14 @@
-import { CacheInterceptor } from "@/infrastructures/cache";
-import { UseInterceptors } from "@nestjs/common";
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
-import { Cat, CatDto } from "./cat.schema";
+import { Resolver } from "@nestjs/graphql";
 import { CatsService } from "./cat.service";
 
 @Resolver()
 export class CatResolver {
 	constructor(private readonly catService: CatsService) {}
 
-	@Mutation(() => Cat)
-	@UseInterceptors(CacheInterceptor)
-	async createNewCat(@Args({ name: "cat", type: () => CatDto }) catDto: CatDto) {
-		const newCat = await this.catService.create(catDto);
-		return newCat;
-	}
+	// @Mutation(() => Cat)
+	// @UseInterceptors(CacheInterceptor)
+	// async createNewCat(@Args({ name: "cat", type: () => CatDto }) catDto: CatDto) {
+	// 	const newCat = await this.catService.create(catDto);
+	// 	return newCat;
+	// }
 }
